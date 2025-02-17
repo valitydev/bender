c01e3cac68a07b154f818c08174821f54715d72b
Aleksey Kashapov <nanodirijabl@gmail.com>
2025-02-17 19:37:13 +0300

    Implements hybrid machinery backend

--- apps/bender/test/bender_tests_SUITE.erl
+++ apps/bender/test/bender_tests_SUITE.erl
@@ -65,10 +65,75 @@ groups() ->
 
 -spec init_per_suite(config()) -> config().
 init_per_suite(C) ->
+    %% _ = dbg:tracer(),
+    %% _ = dbg:p(all, c),
+    %% _ = dbg:tpl({'progressor', 'get', '_'}, x),
+    EpgConnectorApps = genlib_app:start_application_with(epg_connector, [
+        {databases, #{
+            default_db => #{
+                host => "postgres",
+                port => 5432,
+                database => "progressor_db",
+                username => "progressor",
+                password => "progressor"
+            }
+        }},
+        {pools, #{
+            default_pool => #{
+                database => default_db,
+                size => 30
+            }
+        }}
+    ]),
+    ProgressorApps = genlib_app:start_application_with(progressor, [
+        {call_wait_timeout, 20},
+        {defaults, #{
+            storage => #{
+                client => prg_pg_backend,
+                options => #{
+                    pool => default_pool
+                }
+            },
+            retry_policy => #{
+                initial_timeout => 5,
+                backoff_coefficient => 1.0,
+                %% seconds
+                max_timeout => 180,
+                max_attempts => 3,
+                non_retryable_errors => []
+            },
+            task_scan_timeout => 1,
+            worker_pool_size => 100,
+            process_step_timeout => 30
+        }},
+        {namespaces, #{
+            'bender_generator' => #{
+                processor => #{
+                    client => machinery_prg_backend,
+                    options => #{
+                        namespace => 'bender_generator',
+                        handler => {bender_generator, #{}},
+                        schema => machinery_mg_schema_generic
+                    }
+                }
+            },
+            'bender_sequence' => #{
+                processor => #{
+                    client => machinery_prg_backend,
+                    options => #{
+                        namespace => 'bender_sequence',
+                        handler => {bender_sequence, #{}},
+                        schema => machinery_mg_schema_generic
+                    }
+                }
+            }
+        }}
+    ]),
     ScoperApps = genlib_app:start_application_with(scoper, [
         {storage, scoper_storage_logger}
     ]),
     BenderApps = genlib_app:start_application_with(bender, [
+        {machinery_backend, hybrid},
         {generator, #{
             path => <<"/v1/stateproc/bender_generator">>,
             schema => machinery_mg_schema_generic,
@@ -95,7 +160,7 @@ init_per_suite(C) ->
             num_acceptors => 100
         }}
     ]),
-    Apps = ScoperApps ++ BenderApps,
+    Apps = EpgConnectorApps ++ ProgressorApps ++ ScoperApps ++ BenderApps,
     [{suite_apps, Apps} | C].
 
 -spec end_per_suite(config()) -> ok.
