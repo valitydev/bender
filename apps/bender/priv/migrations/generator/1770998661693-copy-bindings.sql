1db7b47534b7fc9e33b2a905b1710ed07156bfa5
ttt161 <losto@nix>
2026-04-13 14:03:14 +0300

    fix generator migration

--- apps/bender/priv/migrations/generator/1770998661693-copy-bindings.sql
+++ apps/bender/priv/migrations/generator/1770998661693-copy-bindings.sql
@@ -1,1 +1,1 @@
-INSERT INTO bender_generator_states (id, state) SELECT process_id, aux_state FROM bender_generator_processes;
+INSERT INTO bender_generator_states (id, state) SELECT process_id, aux_state FROM bender_generator_processes WHERE aux_state NOT NULL;
